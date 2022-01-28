c93f8990056dc015db8da2f2ec3e0aeb6a1647ce
Allison Stewart <amstewart@yahoo.com>
2022-01-28 15:41:33 -0500

    all tests passing

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -7,15 +7,30 @@ import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
+// const testArticle = {
+//   createdOn: '2021-08-09T18:02:38-04:00',
+//   img: 'https://picsum.photos/id/134/300/300',
+//   headline: 'test headline',
+//   author: 'test author',
+//   summary: 'test summary', 
+//   body: 'test body'
+// }
+
 test('renders component without errors', ()=> {
-  render (<Article />)
+  render (<Article article={{
+    createdOn: '2021-08-09T18:02:38-04:00',
+    img: 'https://picsum.photos/id/134/300/300',
+    headline: 'test headline',
+    author: 'test author',
+    summary: 'test summary', 
+    body: 'test body'
+  }}/>)
 });
 
 test('renders headline, author from the article when passed in through props', ()=> {
   render (<Article article={{headline: 'Headline Here', author: 'Author here'}}/>);
 
   const headline = screen.queryByText(/headline here/i);
-  console.log(headline)
   const author = screen.queryByText(/author here/i);
 
   expect(headline).toBeInTheDocument();
@@ -28,9 +43,29 @@ test('renders headline, author from the article when passed in through props', (
 });
 
 test('renders a"Associated Press" when no author is given', ()=> {
+  render(<Article article={{author: ''}}/>)
+
+  const author = screen.queryByTestId('author');
+  
+  expect(author).toHaveTextContent(/associated press/i);
 });
 
 test('executes handleDelete when the delete button is pressed', ()=> {
+  const mockHandleDelete = jest.fn();
+
+  render (<Article article={{
+    createdOn: '2021-08-09T18:02:38-04:00',
+    img: 'https://picsum.photos/id/134/300/300',
+    headline: 'test headline',
+    author: 'test author',
+    summary: 'test summary', 
+    body: 'test body'
+  }} handleDelete={mockHandleDelete}/>)
+
+  const deleteButton = screen.queryByTestId('deleteButton');
+  userEvent.click(deleteButton)
+
+  expect(mockHandleDelete).toHaveBeenCalled();
 });
 
 //Task List: 
