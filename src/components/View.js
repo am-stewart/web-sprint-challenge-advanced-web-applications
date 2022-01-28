084cd140ceb289d7f2eac33f11a94de798a3ce5d
Allison Stewart <amstewart@yahoo.com>
2022-01-28 11:32:32 -0500

    articles set to state on mount

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axiosWithAuth from './../utils/axiosWithAuth';
 
 import Article from './Article';
 import EditForm from './EditForm';
@@ -8,6 +9,17 @@ const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
+
+    useEffect(()=> {
+        axiosWithAuth()
+        .get('/articles')
+        .then(resp => {
+            setArticles(resp.data)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
 
     const handleDelete = (id) => {
     }
